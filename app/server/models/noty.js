beb275de4cdee6ab5f38003e53ee5a7a4f4b0a99
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-08-24 20:35:57 +0300

    Added Noty entity

---
+++ app/server/models/noty.js
@@ -0,0 +1,56 @@
+const UuidEntity = require('./uuidentity');
+const User = require('./user');
+
+class Noty extends UuidEntity
+{
+	constructor({
+		uuid,
+		name = 'A Noty',
+		owner = null,
+		content = null,
+		files = []
+	} = {})
+	{
+		super({ uuid });
+		this.name = name;
+		this.owner = owner;
+		this.content = content;
+		this.files = files;
+	}
+
+	serialize()
+	{
+		const data = super.serialize();
+		data.name = this.name;
+		data.owner = this.owner;
+		data.content = this.content;
+		data.files = this.files;
+	}
+
+	extractFiles(dbConnection)
+	{
+		let p = Promise.resolve([]);
+		for (let i = 0; i < this.files.length; i++)
+		{
+			p = p.then(arr =>
+			{
+				arr.push(Entity.extract(dbConnection, User, { uuid: this.files[i] }));
+				return arr;
+			});
+		}
+		return p;
+	}
+
+	extractOwner(dbConnection)
+	{
+		return Entity.extract(dbConnection, User, { uuid: this.owner });
+	}
+}
+Noty.collection = 'noties';
+
+Noty.extract = function (dbConnection, uuid)
+{
+	return Entity.extract(dbConnection, Noty, { uuid });
+}
+
+module.exports = Noty;
